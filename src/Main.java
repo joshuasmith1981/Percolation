ea5574331db6d7e138cb1b0fed09fcacd4ec1271
Joshua Smith <joshua.l.smith@nordstrom.com>
2014-02-14 09:01:02 -0800

    Percolation work-in-progress.

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,41 @@
 
 public class Main {
-	private static final int N = 5;
+	private static final int N = 20;
 	
 	private static final Percolation perk = new Percolation(N);
 	
 	public static void main(String[] args) {
 		
-		//Percolation perk = new Percolation(N);
+		int x1 = 3;
+		int y1 = 1;
 		
-		int x1 = 2;
-		int y1 = 2;
+		int x2 = 3;
+		int y2 = 2;
 		
-		int x2 = 2;
-		int y2 = 3;
+		int x3 = 3;
+		int y3 = 3;
 		
-		int x3 = 4;
-		int y3 = 4;
-		
-		int x4 = 5;
-		int y4 = 4;
-		
+		int x4 = 4;
+		int y4 = 5;
+				
 		perk.open(x1, y1);
 		perk.open(x2, y2);
 		perk.open(x3, y3);
+		perk.open(3, 4);
+		perk.open(4, 4);
 		perk.open(x4, y4);
-				
-		writeConnectedStateToConsole(x1, y1, x2, y2);
-		writeConnectedStateToConsole(x1, y1, x3, y3);
-		writeConnectedStateToConsole(x3, y3, x4, y4);
+		System.out.println();
 		
+		perk.printGrid();
+		perk.printOpenSites();
+		
+		writeFullStateToConsole(x2, y2);
+		writeFullStateToConsole(x4, y4);
+		
+		boolean percolates = perk.percolates();
+		
+		System.out.println();
+		System.out.println(String.format("System %s percolate!", percolates ? "does" : "does not"));
 	}
 	
 	private static int xyToID(int x, int y) {
@@ -41,5 +48,13 @@ public class Main {
 		boolean connected = perk.connected(id1, id2);
 		
 		System.out.println(String.format("Points (%s, %s) and (%s, %s) %s connected.", x1, y1, x2, y2, connected ? "are" : "are not"));
+		System.out.println();
+	}
+	
+	private static void writeFullStateToConsole(int x, int y) {
+		boolean siteFull = perk.isFull(x, y);
+		
+		System.out.println(String.format("Site at point (%s, %s) is %s.", x, y, siteFull ? "full" : "not full"));
+		System.out.println();
 	}
 }
--- src/Percolation.java
+++ src/Percolation.java
@@ -8,17 +8,18 @@ public class Percolation {
 	private final int BLOCKED_SITE = 0;
 	
 	private int N;
+	private int nSquared;
 	private int[][] grid;
 	private int[] openSites;
 	private WeightedQuickUnionUF lookup;
-
+	
 	public Percolation(int N) {
 		this.N = N;
-		int nSquared = (int)Math.pow(N, 2);
+		this.nSquared = (int)Math.pow(N, 2);
 		
-		grid = new int[N][N];
-		openSites = new int[nSquared];
-		lookup = new WeightedQuickUnionUF(nSquared);
+		grid = new int[N + 1][N+ 1];
+		openSites = new int[nSquared + 1];
+		lookup = new WeightedQuickUnionUF(nSquared + 1);
 	}
 	
 	public void open(int i, int j) {
@@ -34,9 +35,9 @@ public class Percolation {
 			System.out.println();
 			
 			boolean leftOpen = (i > MIN_INDEX && isOpen(i - 1, j));
-			boolean rightOpen = (i < N - 1 && isOpen(i + 1, j));
+			boolean rightOpen = (i < N && isOpen(i + 1, j));
 			boolean aboveOpen = (j > MIN_INDEX && isOpen(i, j - 1));
-			boolean belowOpen = (j < N - 1 && isOpen(i, j + 1));
+			boolean belowOpen = (j < N && isOpen(i, j + 1));
 			
 			if (!leftOpen && !rightOpen && !aboveOpen && !belowOpen) {
 				System.out.println(String.format("No open neighbors found for site with ID %s.", id));
@@ -46,28 +47,32 @@ public class Percolation {
 			
 			if (leftOpen) {
 				int idToLeft = xyToID(i - 1, j);
-				connect(id, idToLeft);
+				//connect(id, idToLeft);
+				connect(idToLeft, id);
 				System.out.println(String.format("Connected to open site to the left (ID %s).", idToLeft));
 				System.out.println();
 			}
 			
 			if (rightOpen) {
 				int idToRight = xyToID(i + 1, j);
-				connect(id, idToRight);
+				//connect(id, idToRight);
+				connect(idToRight, id);
 				System.out.println(String.format("Connected to open site to the right (ID %s).", idToRight));
 				System.out.println();
 			}
 			
 			if (aboveOpen) {
 				int idAbove = xyToID(i, j - 1);
-				connect(id, idAbove);
+				//connect(id, idAbove);
+				connect(idAbove, id);
 				System.out.println(String.format("Connected to open site above (ID %s).", idAbove));
 				System.out.println();
 			}
 			
 			if (belowOpen) {
 				int idBelow = xyToID(i, j + 1);
-				connect(id, idBelow);
+				//connect(id, idBelow);
+				connect(idBelow, id);
 				System.out.println(String.format("Connected to open site below (ID %s).", idBelow));
 				System.out.println();
 			}
@@ -83,10 +88,50 @@ public class Percolation {
 	public boolean isFull(int i, int j) {
 		validateIndices(i, j);
 		
-		return false;
+		int id = xyToID(i, j);
+		
+		int root = lookup.find(id);
+		
+		System.out.println(String.format("Found component root %s using ID %s (calculated from point (%s, %s))", root, id, i, j));
+		
+		boolean isFull = false;
+		
+		for (int index = 1; index <= N; index++) {
+			if (openSites[index] == OPEN_SITE) {
+				if (lookup.connected(id, index)) {
+					isFull = true;
+					break;
+				}
+			}
+		}
+		
+		return isFull;
 	}
 	
 	public boolean percolates() {
+				
+		int[] openTopSites = new int[N];
+		
+		for (int i = 1; i <= N; i++) {
+			if (openSites[i] == OPEN_SITE) {
+				openTopSites[i] = i;
+			}
+		}
+		
+		for (int i = nSquared - N; i <= nSquared; i++) {
+			int bottomSite = lookup.find(i);
+			
+			if (openSites[bottomSite] == OPEN_SITE) {
+				for (int j = 0; j < openTopSites.length; j++) {
+					int topSite = openTopSites[j];
+					
+					if (lookup.connected(bottomSite, topSite)) {
+						return true;
+					}
+				}
+			}
+		}
+		
 		return false;
 	}
 	
@@ -94,6 +139,25 @@ public class Percolation {
 		return lookup.connected(p, q);
 	}
 	
+	public void printGrid() {
+		for (int y = 1; y <= N; y++) {
+			for (int x = 1; x <= N; x++) {
+				System.out.print(grid[x][y]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public void printOpenSites() {
+		for (int i = 1; i <= nSquared; i++) {
+			System.out.print(openSites[i]);
+		}
+		
+		System.out.println();
+		System.out.println();
+	}
+		
 	private void connect(int id1, int id2) {
 		lookup.union(id1, id2);
 	}
